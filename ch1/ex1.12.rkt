473a9710ee6da0e0dbc4b464e9ab44a8838027e0
Asheet Kumar <ug201210006@iitj.ac.in>
2016-08-05 15:13:57 +0530

    Excercise  problem 1.12

---
+++ ch1/ex1.12.rkt
@@ -0,0 +1,29 @@
+#lang scheme
+
+; pascal's triangle
+; rows 0,1,2,3,.....
+; elements  0,1,2,3,4,.........
+; eth element in rth row is rCe   (P&C)
+
+(define (fact n)
+     (if(<= n 1)
+          1
+          (* n (fact (- n 1)))
+        )
+  )
+
+
+
+
+
+(define (rth-eth  r e )
+    (if (<= e (+ r 1))
+      (/ (fact r) (* (fact 2) (fact (- r e))))
+      0
+    )
+  )
+
+(define r 4)
+(define e 6)
+
+(rth-eth r e)
