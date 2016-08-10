f20584422d7dd3fc7abb02d398156f4a9f92a2b7
Asheet Kumar <ug201210006@iitj.ac.in>
2016-08-10 12:52:06 +0530

    ex1.46

---
+++ ch1/ex1.46.rkt
@@ -0,0 +1,43 @@
+#lang scheme
+
+(define tolerance 0.001)
+
+(define (sq x)(* x x))
+
+(define (avg a b)(/ (+ a b) 2))
+
+(define (iterative-improve good-enough? improve )
+  (define (iter-imp x)
+    (if(good-enough? x)
+         x
+         (iter-imp (improve x))
+       )
+    ) iter-imp
+  )
+ 
+(define (sqrt x)
+  ((iterative-improve
+   (lambda (guess) (< (abs (- (sq guess) x))tolerance))
+   (lambda (guess) (avg guess (/ x guess)))
+   ) 1.0)
+  )
+
+(define (fixed-point f )
+   ((iterative-improve
+   (lambda (guess) (< (abs (- (f guess) guess))tolerance))
+   (lambda (guess) (f guess))
+   ) 1.0))
+  
+
+(fixed-point (lambda (x)(+ 1 (/ 1 x))) )
+
+(sqrt 16)
+
+(define (cube x)
+  ((iterative-improve
+   (lambda (g) (< (abs (- (* g  g) x))tolerance))
+   (lambda (g) (avg g (/ x g)))
+   ) 1.0)
+  )
+
+(cube 27)
