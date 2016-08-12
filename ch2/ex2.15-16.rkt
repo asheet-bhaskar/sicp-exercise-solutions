1e7e1c93590fdec09a13105b2e7939253450dbda
Asheet Kumar <ug201210006@iitj.ac.in>
2016-08-12 15:49:44 +0530

    ex2.15 - 16

---
+++ ch2/ex2.15-16.rkt
@@ -0,0 +1,16 @@
+#lang scheme
+
+#|
+  let n be a fixed number and i be an interval,
+  than n/n will be equal to 1. whereas i/i will not be equal to 1.  --(1)
+
+  in part1 equivalent resistance is calculated using (R1R2)/(R2+R1) whereas in
+  part2 equivalent resistance is calculated using 1/(1/R1 + 1/R2). --(2)
+
+  in part1 variables with uncertain value have more occurance than in part2.
+  considering  case --(1) we can see that i/i is not equal to 1 so with more
+  occurances error will be more that is why part2 is more accurate than part1
+
+  An imroved interval-arithmetic package can be developed. But still it will have some error
+  
+|#
