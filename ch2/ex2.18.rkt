f0acace38419ec7284087b41c63338522c61c6ce
Asheet Kumar <ug201210006@iitj.ac.in>
2016-08-12 15:46:44 +0530

    ex2.18

---
+++ ch2/ex2.18.rkt
@@ -0,0 +1,17 @@
+#lang scheme
+
+(define (list-reverse items rev)
+    (if (null? items)
+         rev
+         (list-reverse (cdr items) (cons (car items) rev))
+        )
+  )
+(define (list-length list)
+  (if(null? list)
+     0
+     (+ 1 (list-length(cdr list))))
+  )
+
+(define rev (list ))
+(define list1 (list 1 2 3 4 5 6 7 8))
+(list-reverse list1 rev )
