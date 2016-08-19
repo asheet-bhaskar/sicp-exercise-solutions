0de4232e10fb4dd6ca7af28c713903fdefee534a
Asheet Kumar <ug201210006@iitj.ac.in>
2016-08-19 15:24:13 +0530

    updated deep reverse for lists (up to nth level deep)

--- ch2/ex2.27.rkt
+++ ch2/ex2.27.rkt
@@ -1,5 +1,5 @@
 #lang scheme
-
+(require racket/trace)
 
 (define (list-reverse items rev)
     (if (null? items)
@@ -13,18 +13,37 @@
   (if(null? items)
       rev
       (begin
-        (if(list? (car items))
+        (if(pair? (car items))
            (deep-reverse (cdr items) (cons (list-reverse (car items) null) rev) )
            (deep-reverse (cdr items) (cons (car items) rev)))
            )
         
-        )) 
+        ))
+
+(define (deep-reverse2 items)
+  (define (iter items rev)
+    (cond
+      ((null? items) rev)
+      ((pair? (car items)) (iter (cdr items) (cons (iter (car items) null) rev)))
+      (else
+        (iter (cdr items) (cons (car items) rev))
+       )
+       ))
+   (iter items null)
+  )
+
+
+
+
 
 
 (define rev (list ))
-(define list1 (list 1 2 11 13 (list 3 -1 4 0) (list 5 6 7 8)))
+(define list1 (list 1 2  (list 3 -1 ) (list 5 (list 6 (list 7 -7)) 8)))
 (list-reverse list1 rev )
 
 (deep-reverse list1 rev)
 (newline)
 list1
+(newline)
+(trace deep-reverse2)
+(deep-reverse2 list1)
